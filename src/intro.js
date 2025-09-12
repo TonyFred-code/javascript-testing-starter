605cb80cd12ee909101d52e80382ac5a0c4a47a7
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-12 05:39:26 +0100

    feat: create factorial function by test specs

--- src/intro.js
+++ src/intro.js
@@ -18,3 +18,11 @@ export function calculateAverage(numbers) {
 
   return sum / numbers.length;
 }
+
+// TDD exercise
+export function factorial(number) {
+  if (number < 0) return NaN;
+  if (number == 0 || number == 1) return 1;
+
+  return number * factorial(number - 1);
+}
