0931c12acb24e454e96c67e8f47e4dbb918cc436
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-12 05:18:39 +0100

    test: add test for fizzBuzz function

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro.js";
+import { fizzBuzz, max } from "../src/intro.js";
 
 describe("max", () => {
   test("should return the first argument if it is greater", () => {
@@ -12,5 +12,25 @@ describe("max", () => {
 
   test("should return the first argument if arguments are equal", () => {
     expect(max(2, 2)).toBe(2);
+  });
+});
+
+// Exercise:
+
+describe("fizzBuzz", () => {
+  test("should return FizzBuzz if number is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  test("should return Fizz if number is only divisible by 3", () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+
+  test("should return Buzz if number is only divisible by 5", () => {
+    expect(fizzBuzz(20)).toBe("Buzz");
+  });
+
+  test("should return number as string if number is not divisible by 3 or 5", () => {
+    expect(fizzBuzz(4)).toBe("4");
   });
 });
