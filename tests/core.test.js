bb8d315bafbf00a811cee53952e06c630b5cc584
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-13 22:45:40 +0100

    test: update isPriceInRange test suite
    use parameterized testing for repeated testing logic

--- tests/core.test.js
+++ tests/core.test.js
@@ -96,17 +96,34 @@ describe("validateUserInput", () => {
 });
 
 describe("isPriceInRange", () => {
-  test("should return false when the price is outside the range", () => {
-    expect(isPriceInRange(-10, 0, 100)).toBe(false);
-    expect(isPriceInRange(200, 0, 100)).toBe(false);
-  });
-
-  test("should return true when the price is equal to the min or to the max", () => {
-    expect(isPriceInRange(0, 0, 100)).toBe(true);
-    expect(isPriceInRange(100, 0, 100)).toBe(true);
-  });
-  test("should return true when the price is within the range", () => {
-    expect(isPriceInRange(50, 0, 100)).toBe(true);
+  test.each([
+    {
+      price: -10,
+      scenario: "price < min",
+      result: false,
+    },
+    {
+      price: 0,
+      result: true,
+      scenario: "price = min",
+    },
+    {
+      price: 50,
+      result: true,
+      scenario: "price between min and max",
+    },
+    {
+      price: 100,
+      result: true,
+      scenario: "price = max",
+    },
+    {
+      price: 200,
+      result: false,
+      scenario: "price > max",
+    },
+  ])("should return $result when $scenario", ({ price, result }) => {
+    expect(isPriceInRange(price, 0, 100)).toBe(result);
   });
 });
 
